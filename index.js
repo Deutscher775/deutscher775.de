1235eebf7e7042b9cb3f00c655ea0ccfe7c72463
Jason <81298350+Deutscher775@users.noreply.github.com>
2025-02-02 00:12:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-window.addEventListener('load', async function() {
-    await new Promise(resolve => setTimeout(resolve, 1000));
 
-    function reloadIFrame() {
-        const iframe = document.querySelector('.discord-activity');
-        if (iframe) {
-            iframe.contentWindow.location = iframe.src;
-        } else {
-            console.error('iframe not found');
-        }
-    }
-    
-    window.setInterval(reloadIFrame, 5000); 
-});
